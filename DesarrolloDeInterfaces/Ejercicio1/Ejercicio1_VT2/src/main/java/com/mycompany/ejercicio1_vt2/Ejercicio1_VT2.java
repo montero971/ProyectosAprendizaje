235deae868165dfec7de4eb9a3414b8ad578e94f
JMMONTERO-PC\josemanuel <montero97.jmm@gmail.com>
2022-10-02 17:27:43 +0200

    Creacion del proyecto con NetBeans.

---
+++ DesarrolloDeInterfaces/Ejercicio1/Ejercicio1_VT2/src/main/java/com/mycompany/ejercicio1_vt2/Ejercicio1_VT2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio1_vt2;
+
+/**
+ *
+ * @author josemanuel
+ */
+public class Ejercicio1_VT2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
